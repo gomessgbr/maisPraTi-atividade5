cb100fa11b206c135498931a4aca2349386829eb
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-08-15 19:08:44 -0300

    feat: error boundery

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.jsx";
-
+import { ErrorBoundary } from "react-error-boundary";
+import { ErrorFallBack } from "./components/index.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary fallback={<ErrorFallBack/>}>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
