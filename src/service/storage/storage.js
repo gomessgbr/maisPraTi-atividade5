98c966d6698d8804c72dbe63b97539a697fe412f
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-08-21 18:36:14 -0300

    docs: manage storage

--- src/service/storage/storage.js
+++ src/service/storage/storage.js
@@ -1,13 +1,14 @@
-// Seta 
+// Seta um valor para algum serviço de storage, nesse caso localStorage
 export function setStorage(key, data){
   return localStorage.setItem(key, data);
 }
-
+// Pega um valor de algum serviço de storage, nesse caso localStorage
 export function getStorage(key){
   return localStorage.getItem(key);
 
 }
 
+// remove um valor de algum serviço de storage, nesse caso localStorage
 export function removeStorage(key){
   return localStorage.removeItem(key);
 }
