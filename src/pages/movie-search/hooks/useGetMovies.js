ac9288d090cb1a34b41f5ac12e188ad2286d7d04
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-08-21 21:47:17 -0300

    feat: change api for fetch movies

--- src/pages/movie-search/hooks/useGetMovies.js
+++ src/pages/movie-search/hooks/useGetMovies.js
@@ -1,21 +1,46 @@
-import axios from "axios";
+// import axios from "axios";
 import { useState } from "react";
+import { makeRequest } from "../../../service/api/httpRequestProvider";
 
 // Hook para buscar filmes
 export function useGetMovies() {
   const [movies, setMovies] = useState([]); // Define o estado para armazenar os filmes
+  const [errors, setErrors] = useState({ show: false, message: "" });
 
   // Função para buscar filmes
-  const searchMovies = async ({query}) => {
+  const searchMovies = async (query) => {
     try {
-      const response = await axios.get(
-        `http://www.omdbapi.com/?s=${query}&apikey=403abbfe`
-      ); // Faz uma requisição GET para a API OMDB
-      setMovies(response.data.Search); // Armazena os dados dos filmes no estado movies
+      const url = `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=140606622f4c3ba1ee78f626ae9d4b16`;
+      const data = await makeRequest("get", url);
+
+      // Base URL para as imagens do TMDb
+      const baseUrl = "https://image.tmdb.org/t/p/";
+      const imageSize = "w300"; // Você pode ajustar o tamanho conforme necessário
+
+      // Mapeia os resultados para incluir a URL completa da imagem
+      const moviesWithImages = data.results.map((movie) => ({
+        ...movie,
+        posterUrl: movie.poster_path
+          ? `${baseUrl}${imageSize}${movie.poster_path}`
+          : null,
+        backdropUrl: movie.backdrop_path
+          ? `${baseUrl}${imageSize}${movie.backdrop_path}`
+          : null,
+      }));
+
+      setMovies(moviesWithImages); // Armazena os dados dos filmes, incluindo as URLs das imagens
     } catch (error) {
-      console.error("Error fetching movie data:", error); // Exibe um erro no console em caso de falha
+      onError(); // Exibe um erro na tela para usuário
     }
   };
 
-  return { movies, searchMovies };
+  const onError = () => {
+    setErrors({ show: true, message: "Não foi achado nenhum filme" });
+  };
+
+  const hideErrors = () => {
+    setErrors({ show: false, message: "" });
+  };
+
+  return { movies, searchMovies, hideErrors, errors };
 }
