adf0e686d68108c0a4590a5e4162fe359eff19b7
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-08-21 21:04:02 -0300

    feat: function to request api

---
+++ src/service/api/httpRequestProvider.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+// Uma função que usa uma lib para fazer request, dá para chamar qualquer api usando essa função
+export const makeRequest = async (
+  method,
+  url,
+  data = {},
+  params = {},
+  headers = {}
+) => {
+  try {
+    const response = await axios({
+      method,
+      url,
+      data,
+      params,
+      headers,
+    });
+    return response.data; // Retorna os dados da resposta
+  } catch (error) {
+    // Tratamento genérico de erro
+    if (error.response) {
+      console.error(
+        "Erro na resposta da API:",
+        error.response.status,
+        error.response.data
+      );
+
+      throw new Error(`Erro: ${error.response.status}`);
+    } else if (error.request) {
+      console.error("Nenhuma resposta recebida:", error.request);
+      throw new Error("Nenhuma resposta recebida da API");
+    } else {
+      console.error("Erro ao configurar a requisição:", error.message);
+      throw new Error("Erro de requisição");
+    }
+  }
+};
