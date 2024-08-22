3ba2ba6a85b62d4a33590a69fc9da5b49df565a7
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-08-21 21:46:56 -0300

    feat: error label component

---
+++ src/components/ErrorLabel/ErrorLabelStyles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+
+export const ErrorLabelContainer = styled.div`
+
+
+`
+
+export const ErrorLabelMessage = styled.span`
+  color: red;
+`
--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ export * from "./NavigationBar/NavigationBar";
 export * from "./Footer/Footer";
 export * from "./Carousel/Carousel";
 export * from "./Layout/Layout";
-export * from "./ErrorFallBack/ErrorFallBack"
+export * from "./ErrorFallBack/ErrorFallBack";
+export * from "./ErrorLabel/ErrorLabel";
